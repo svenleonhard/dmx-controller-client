287beb7a7d33d14cadf53034b16dcdf4f73c86ec
svenleonhard <sven.leonhard@7lofficial.de>
2019-10-05 17:56:20 +0200

    introduced fixture cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DmxControllerService } from './dmx-controller.service';
+import { Fixture } from './dto/fixture';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,10 @@ import { DmxControllerService } from './dmx-controller.service';
 })
 export class AppComponent {
   title = 'DMX Controller Client';
-  constructor(public controller: DmxControllerService) { }
+
+  fixtures: Array<Fixture> = [];
+
+  constructor(public controller: DmxControllerService) {
+
+   }
 }
